1bcfe6fcfcbd3706d73afa1f874500c27ba4d8b9
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-01-10 21:54:01 -0800

    Replaced home image with responsive youtube embed.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,28 +1,34 @@
 import "./home.scss";
-import { 
-  Container,
+import {
+ Container,
 } from 'react-bootstrap/'
 import Map from "../map/Map";
 import Welcome from "../welcome/Welcome";
 import Projects from "../projects/Projects";
 import Joinus from "../joinus/Joinus";
 import Links from "../links/Links";
-
+ 
 export default function Home() {
-
-  return (
-    <>
-    <Container className="home content-container pt-4" id="home" fluid="true">
-      <img
-        src="https://allnationsafricanchurch.com/wp-content/uploads/2017/08/thecorss.jpg"
-        alt="Cross"
-      />
-      </Container>
-    <Welcome/>
-    <Links/>
-    <Joinus/>
-    <Map/>
-    <Projects/>
-    </>
-  )
+ 
+ return (
+   <>
+   <Welcome/>
+   <Container className="home content-container d-flex justify-content-center py-4" id="home" fluid="true">
+     <iframe
+       width="560"
+       height="315"
+       src="https://www.youtube.com/embed/bDOEkIxqQPA?start=1&autoplay=1&mute=1"
+       title="YouTube video player"
+       frameborder="0"
+       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+       allowfullscreen
+     >
+     </iframe>
+   </Container>
+   <Links/>
+   <Joinus/>
+   <Map/>
+   <Projects/>
+   </>
+ )
 }
