428b1fc979d0492db16c6537b2236232a290a5f4
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-04-07 14:24:13 -0700

    Video is now responsive.

--- src/components/joinus/Joinus.jsx
+++ src/components/joinus/Joinus.jsx
@@ -13,11 +13,11 @@ export default function Joinus() {
 
   return (
     <>
-      <Container className="joinus content-container py-4 " id="home" fluid="true">
+      <Container className="joinus content-container py-4" id="home" fluid="false">
       <FadeIn>
-        <Container className="hours content-container py-4 px-4" id="home" fluid="true">
+        <Container className="hours content-container py-4 px-4" id="home" fluid="false">
             <Row>
-              <Col className="d-flex justify-content-center py-4" lg={4} md={12} sm={12}>
+              <Col className="d-flex justify-content-center py-4" lg={4} md={12} sm={12} xs={12}>
                 <Card className="card-size">
                 <Card.Title className="text-center px-2">Join Us For Worship</Card.Title>
                   <Card.Body className="text-center">
@@ -42,14 +42,13 @@ export default function Joinus() {
                   </Card.Body>
                 </Card>
               </Col>
-              <Col className="map d-flex justify-content-center align-items-center py-4 px-4" lg={8} md={12} sm={12}>
+              <Col className="map d-flex justify-content-center align-items-center py-4" lg={8} md={12} sm={12} xs={12}>
               <Card className="card-size flex-grow-1">
               <Card.Title className="text-center px-2">Latest Message</Card.Title>
                 <Card.Body className="text-center">
                   <iframe
-                    width="560"
                     height="315"
-                    src="https://www.youtube.com/embed/bDOEkIxqQPA?start=1&autoplay=1&mute=1"
+                    src="https://www.youtube.com/embed/c0dahMbWNU4?start=1&autoplay=1&mute=1"
                     frameborder="0"
                     title="Google Maps embed"
                     className=""
