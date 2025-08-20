47f75f307b56bd0841725767c2a009016de09af2
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2025-08-20 06:27:03 -0700

    Latest sermon now spans the entire row.

--- src/components/joinus/Joinus.jsx
+++ src/components/joinus/Joinus.jsx
@@ -16,6 +16,59 @@ export default function Joinus() {
 
         <Container className="hours content-container pt-4 px-4" id="home" fluid="false">
           <Row>
+            <Col className="map d-flex justify-content-center align-items-center py-4" >
+              <Card className="card-size flex-grow-1">
+                <Card.Title className="text-center px-2">Latest Message</Card.Title>
+                <Card.Body className="text-center">
+                  <div className="videoWrapper">
+                    <iframe
+                      height="315"
+                      src="https://www.youtube.com/embed?listType=playlist&list=UU5jYWyaWW3b0r7MUrK1WVxw"
+                      frameBorder="0"
+                      title="Google Maps embed"
+                      className=""
+                      loading="lazy"
+                    >
+                    </iframe>
+                  </div>
+                  <br></br>
+                  <Card.Text className="previous-sermons">
+                    Previous 3 Sermons
+                    <Row>
+                      <Col>
+                        <br></br>
+                        <div className="videoWrapper">
+                          <iframe
+                            title="Previous Sermon 1"
+                            src="https://www.youtube.com/embed?listType=playlist&list=UU5jYWyaWW3b0r7MUrK1WVxw&index=2">
+                          </iframe>
+                        </div>
+                      </Col>
+                      <Col>
+                        <br></br>
+                        <div className="videoWrapper">
+                          <iframe
+                            title="Previous Sermon 2"
+                            src="https://www.youtube.com/embed?listType=playlist&list=UU5jYWyaWW3b0r7MUrK1WVxw&index=3">
+                          </iframe>
+                        </div>
+                      </Col>
+                      <Col>
+                        <br></br>
+                        <div className="videoWrapper">
+                          <iframe
+                            title="Previous Sermon 3"
+                            src="https://www.youtube.com/embed?listType=playlist&list=UU5jYWyaWW3b0r7MUrK1WVxw&index=4">
+                          </iframe>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
             <Col className="d-flex justify-content-center pt-4" lg={6} md={12} sm={12} xs={12}>
               <Card className="card-size">
                 <Card.Title className="text-center px-2">Join Us For Worship</Card.Title>
@@ -24,7 +77,7 @@ export default function Joinus() {
                     Wednesday
                   </Card.Subtitle>
                   <ListGroup variant="flush">
-                    <ListGroup.Item>MID-WEEK PRAYER MEETING: <span style={{color: "maroon"}}>TBD...</span></ListGroup.Item>
+                    <ListGroup.Item>MID-WEEK PRAYER MEETING: <span style={{ color: "maroon" }}>TBD...</span></ListGroup.Item>
                     <ListGroup.Item></ListGroup.Item>
                   </ListGroup>
                   <br></br>
@@ -32,7 +85,7 @@ export default function Joinus() {
                     Friday
                   </Card.Subtitle>
                   <ListGroup variant="flush">
-                    <ListGroup.Item>"PAUSE" - EVENING WORSHIP: <span style={{color: "maroon"}}>7:00 P.M.</span></ListGroup.Item>
+                    <ListGroup.Item>"PAUSE" - EVENING WORSHIP: <span style={{ color: "maroon" }}>7:00 P.M.</span></ListGroup.Item>
                     <ListGroup.Item></ListGroup.Item>
                   </ListGroup>
                   <br></br>
@@ -40,11 +93,11 @@ export default function Joinus() {
                     Saturday
                   </Card.Subtitle>
                   <ListGroup variant="flush">
-                    <ListGroup.Item>BREAKFAST: <span style={{color: "maroon"}}>9:00 A.M.</span></ListGroup.Item>
-                    <ListGroup.Item>MORNING PRAYER: <span style={{color: "maroon"}}>9:35 A.M.</span></ListGroup.Item>
-                    <ListGroup.Item>SABBATH SCHOOL: <span style={{color: "maroon"}}>10:00 A.M.</span></ListGroup.Item>
-                    <ListGroup.Item>DIVINE WORSHIP: <span style={{color: "maroon"}}>11:00 A.M.</span></ListGroup.Item>
-                    <ListGroup.Item>POTLUCK LUNCH: <span style={{color: "maroon"}}>1:00 P.M.</span></ListGroup.Item>
+                    <ListGroup.Item>BREAKFAST: <span style={{ color: "maroon" }}>9:00 A.M.</span></ListGroup.Item>
+                    <ListGroup.Item>MORNING PRAYER: <span style={{ color: "maroon" }}>9:35 A.M.</span></ListGroup.Item>
+                    <ListGroup.Item>SABBATH SCHOOL: <span style={{ color: "maroon" }}>10:00 A.M.</span></ListGroup.Item>
+                    <ListGroup.Item>DIVINE WORSHIP: <span style={{ color: "maroon" }}>11:00 A.M.</span></ListGroup.Item>
+                    <ListGroup.Item>POTLUCK LUNCH: <span style={{ color: "maroon" }}>1:00 P.M.</span></ListGroup.Item>
                   </ListGroup>
                   <Card.Text>
                   </Card.Text>
